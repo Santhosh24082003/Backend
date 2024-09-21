6e7fe763ad36c7e68f58671a3f4f0065074d0a40
santhosh24082003 <chendrayasanthosh05@gmail.com>
2024-09-21 17:55:56 +0530

    second commit

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ pdf_files = {
     'C:\\Users\\chend\\Desktop\\New folder\\Mech\\Backend\\kb\\Diesel': 'C:\\Users\\chend\\Desktop\\New folder\\Mech\\Backend\\kb\\Diesel\\Diesel KB.pdf'
 }
 
-@app.route('/uploads', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     result = None
     title_data = []
@@ -168,7 +168,7 @@ def index():
                 'title_data': to_markdown("\n".join(title_data)),
                 'detected_components': detected_components
             }
-    return result
+    return render_template('index.html', result=result, image_url=image_url)
 
 if __name__ == '__main__':
     app.run(debug=False, use_reloader=False)
